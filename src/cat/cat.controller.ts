771478e347ad7ff07d23660402048935a6ee2cd1
Paul Chibueze <paulpadro03@gmail.com>
2023-06-19 10:45:42 +0100

    update

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -6,28 +6,59 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
+  HttpException,
+  UseFilters,
 } from '@nestjs/common';
 import { CatService, PostService } from './cat.service';
 import { CreateCatDto, CreatePostDto } from './dto/create-cat.dto';
 import { UpdateCatDto, UpdatePostDto } from './dto/update-cat.dto';
+import { HttpExceptionFilter } from './http-exception-filter';
+
+class ForbiddenException extends HttpException {
+  constructor() {
+    super('Forbidden', HttpStatus.FORBIDDEN);
+  }
+}
 
 @Controller('cat')
 export class CatController {
   constructor(private readonly catService: CatService) {}
 
+  @UseFilters(new HttpExceptionFilter())
   @Post('create')
   create(@Body() createCatDto: CreateCatDto) {
     return this.catService.create(createCatDto);
   }
 
   @Get()
-  findAll() {
-    return this.catService.findAll();
+  async findAll() {
+    try {
+      await this.catService.findAll();
+    } catch (error) {
+      throw new ForbiddenException();
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.catService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    if (!id || id == '0') {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    }
+    try {
+      await this.catService.findOne(+id);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'This is a custom error',
+        },
+        HttpStatus.BAD_REQUEST,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 
   @Patch(':id')
@@ -37,6 +68,9 @@ export class CatController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {
+    if (!id) {
+      throw new HttpException('Forbidden', HttpStatus.BAD_REQUEST);
+    }
     return this.catService.remove(+id);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 // import { AppModule } from './app.module';
 import { CatModule } from './cat/cat.module';
+import { HttpExceptionFilter } from './cat/http-exception-filter';
 
 const port = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(CatModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(port);
 }
 bootstrap();
